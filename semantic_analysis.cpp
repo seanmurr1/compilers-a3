fed9a050c382783daf8ba99b10f6a104ec7b4102
sean-murray <sean.murray2000@hotmail.com>
2022-10-25 11:18:38 -0400

    fix23

--- semantic_analysis.cpp
+++ semantic_analysis.cpp
@@ -258,9 +258,9 @@ void SemanticAnalysis::visit_return_expression_statement(Node *n) {
   visit(n->get_kid(0));
   // Get expression type
   std::shared_ptr<Type> return_type = n->get_kid(0)->get_type();
-
   // Check if return type can be casted/assigned to function return type
-  check_assignment(m_cur_function->get_base_type(), return_type, n->get_loc());
+  const Location &loc = n->get_loc();
+  check_assignment(m_cur_function->get_base_type(), return_type, loc);
 }
 
 void SemanticAnalysis::visit_function_definition(Node *n) {
